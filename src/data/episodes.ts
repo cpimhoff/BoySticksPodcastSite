a6dce33c841526abc41df369810bb3d5ba6805a4
Dylan Forbes <dylandforbes@gmail.com>
2023-02-26 11:18:02 -0800

    fix episode title (there's no colon) and flesh out description

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -2,9 +2,9 @@ import { Episode } from "./types";
 
 const ALL_EPISODES: Episode[] = [
   {
-    title: "Horizon: Forbidden West",
+    title: "Horizon Forbidden West",
     slug: "horizon-forbidden-west",
-    description: "The sequel to Horizon: Zero Dawn.",
+    description: "The BoySticks gab on for a while about the sequel to Horizon Zero Dawn.",
     timestamp: "2023-02-25",
     fileUrl: "",
   },
