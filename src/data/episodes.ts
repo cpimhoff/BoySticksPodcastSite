efe3afdedde41141a11cfe7a91f15b67bd8e7a6f
Charlie Imhoff <cpimhoff@gmail.com>
2023-03-05 15:41:56 -0500

    Add file url for HFW

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -4,9 +4,11 @@ const ALL_EPISODES: Episode[] = [
   {
     title: "Horizon Forbidden West",
     slug: "horizon-forbidden-west",
-    description: "The BoySticks gab on for a while about the sequel to Horizon Zero Dawn.",
-    timestamp: "2023-02-25",
-    fileUrl: "",
+    description:
+      "The BoySticks gab on for a while about the sequel to Horizon Zero Dawn.",
+    timestamp: "2023-03-05",
+    fileUrl:
+      "https://boysticks-podcast.s3.amazonaws.com/episodes/1_Horizon_Forbidden_West.mp3",
   },
 ];
 
