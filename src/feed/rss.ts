8db74142de0216d72b0e5ed0d54b84a3ec53376e
Charlie Imhoff <cpimhoff@gmail.com>
2023-03-05 20:24:03 -0500

    Attempt with JPG image

--- src/feed/rss.ts
+++ src/feed/rss.ts
@@ -6,7 +6,7 @@ import RSS from "rss";
 async function getRssFeed() {
   const siteUrl = process.env.BASE_URL ?? "localhost";
   const feedUrl = `${siteUrl}/rss.xml`;
-  const albumArt = `${siteUrl}/album.png`;
+  const albumArt = `${siteUrl}/album.jpg`;
 
   const now = new Date();
   const options: RSS.FeedOptions = {
