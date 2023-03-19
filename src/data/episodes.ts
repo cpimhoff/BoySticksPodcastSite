3b1e67e74fd3c5d47f5cca4395d87a7cf90b5d13
Dylan Forbes <dylandforbes@gmail.com>
2023-03-18 18:44:53 -0700

    fix episode order (move episode 2 to the top of the list)

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -2,15 +2,6 @@ import { Episode } from "./types";
 
 const ALL_EPISODES: Episode[] = [
   {
-    title: "Horizon Forbidden West",
-    slug: "horizon-forbidden-west",
-    description:
-      "Join the BoySticks as they make at least one decent point about the sequel to Guerilla Games' \"Horizon Zero Dawn\" between digressions into physics and human reproduction. Unfortunately the game's MarioKart mechanic is not discussed.",
-    timestamp: "2023-03-05",
-    fileUrl:
-      "https://boysticks-podcast.s3.amazonaws.com/episodes/1_Horizon_Forbidden_West.mp3",
-  },
-  {
     title: "God of War: Ragnarok (Part 1)",
     slug: "god-of-war-ragnarok-part-1",
     description:
@@ -19,7 +10,15 @@ const ALL_EPISODES: Episode[] = [
     fileUrl:
       "https://boysticks-podcast.s3.amazonaws.com/episodes/2_God_of_War_Ragnarok_1.mp3"
   },
-
+  {
+    title: "Horizon Forbidden West",
+    slug: "horizon-forbidden-west",
+    description:
+      "Join the BoySticks as they make at least one decent point about the sequel to Guerilla Games' \"Horizon Zero Dawn\" between digressions into physics and human reproduction. Unfortunately the game's MarioKart mechanic is not discussed.",
+    timestamp: "2023-03-05",
+    fileUrl:
+      "https://boysticks-podcast.s3.amazonaws.com/episodes/1_Horizon_Forbidden_West.mp3",
+  },
 ];
 
 export async function getAllEpisodes(): Promise<Episode[]> {
