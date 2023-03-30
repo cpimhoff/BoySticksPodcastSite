ee0e8f550c6e45b9b1fe661a304a4955a3c51676
Dylan Forbes <dylandforbes@gmail.com>
2023-03-29 18:13:39 -0700

    update S3 urls for episodes

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -8,7 +8,7 @@ const ALL_EPISODES: Episode[] = [
       'In this sequel to critically-acclaimed BoySticks episode "God of War: Ragnarok (Part 1)", the BoySticks discuss the second half of the critically-acclaimed sequel "God of War: Ragnarok". It\'s all the analysis and antics you know and love, PLUS the dramatic debut of long-anticipated segment "The D Pad".',
     releasedOn: "2023-03-19",
     fileUrl:
-      "https://boysticks-podcast.s3.amazonaws.com/episodes/2_God_of_War_Ragnarok_2.mp3",
+      "https://boysticks-podcast.s3.amazonaws.com/episodes/02_God_of_War_Ragnarok_2.mp3",
   },
   {
     title: "God of War: Ragnarok (Part 1)",
@@ -17,7 +17,7 @@ const ALL_EPISODES: Episode[] = [
       'Recall your Leviathan Axe and equip your Sigil Arrows, cuz today the BoySticks are tackling "God of War: Ragnarok". There\'s so much plot to discuss that they barely even have time to argue over the important issues, like: Is Faye hot?',
     releasedOn: "2023-03-18",
     fileUrl:
-      "https://boysticks-podcast.s3.amazonaws.com/episodes/2_God_of_War_Ragnarok_1.mp3",
+      "https://boysticks-podcast.s3.amazonaws.com/episodes/02_God_of_War_Ragnarok_1.mp3",
   },
   {
     title: "Horizon Forbidden West",
@@ -26,7 +26,7 @@ const ALL_EPISODES: Episode[] = [
       "Join the BoySticks as they make at least one decent point about the sequel to Guerilla Games' \"Horizon Zero Dawn\" between digressions into physics and human reproduction. Unfortunately the game's MarioKart mechanic is not discussed.",
     releasedOn: "2023-03-05",
     fileUrl:
-      "https://boysticks-podcast.s3.amazonaws.com/episodes/1_Horizon_Forbidden_West.mp3",
+      "https://boysticks-podcast.s3.amazonaws.com/episodes/01_Horizon_Forbidden_West.mp3",
   },
   {
     title: "Test Draft Episode",
