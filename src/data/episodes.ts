c40793392dca83d1b9278c35f02a7d1cd92ccd7e
Dylan Forbes <dylandforbes@gmail.com>
2024-01-15 12:02:23 -0800

    remove colons from 'God of War Ragnarok', and use backticks instead of single quotes to make strings more readable

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -63,19 +63,19 @@ const ALL_EPISODES: Episode[] = [
       "https://boysticks-podcast.s3.amazonaws.com/episodes/03_Final_Fantasy_VII_Remake.mp3",
   },
   {
-    title: "God of War: Ragnarok (Part 2)",
+    title: "God of War Ragnarok (Part 2)",
     slug: "god-of-war-ragnarok-part-2",
     description:
-      'In this sequel to critically-acclaimed BoySticks episode "God of War: Ragnarok (Part 1)", the BoySticks discuss the second half of the critically-acclaimed sequel "God of War: Ragnarok". It\'s all the analysis and antics you know and love, PLUS the dramatic debut of long-anticipated segment "The D Pad".',
+      `In this sequel to critically-acclaimed BoySticks episode "God of War Ragnarok (Part 1)", the BoySticks discuss the second half of the critically-acclaimed sequel "God of War Ragnarok". It's all the analysis and antics you know and love, PLUS the dramatic debut of long-anticipated segment "The D Pad".`,
     releasedOn: "2023-03-19",
     fileUrl:
       "https://boysticks-podcast.s3.amazonaws.com/episodes/02_God_of_War_Ragnarok_2.mp3",
   },
   {
-    title: "God of War: Ragnarok (Part 1)",
+    title: "God of War Ragnarok (Part 1)",
     slug: "god-of-war-ragnarok-part-1",
     description:
-      'Recall your Leviathan Axe and equip your Sigil Arrows, cuz today the BoySticks are tackling "God of War: Ragnarok". There\'s so much plot to discuss that they barely even have time to argue over the important issues, like: Is Faye hot?',
+      `Recall your Leviathan Axe and equip your Sigil Arrows, cuz today the BoySticks are tackling "God of War Ragnarok". There's so much plot to discuss that they barely even have time to argue over the important issues, like: Is Faye hot?`,
     releasedOn: "2023-03-18",
     fileUrl:
       "https://boysticks-podcast.s3.amazonaws.com/episodes/02_God_of_War_Ragnarok_1.mp3",
@@ -84,7 +84,7 @@ const ALL_EPISODES: Episode[] = [
     title: "Horizon Forbidden West",
     slug: "horizon-forbidden-west",
     description:
-      "Join the BoySticks as they make at least one decent point about the sequel to Guerilla Games' \"Horizon Zero Dawn\" between digressions into physics and human reproduction. Unfortunately the game's MarioKart mechanic is not discussed.",
+      `Join the BoySticks as they make at least one decent point about the sequel to Guerilla Games' "Horizon Zero Dawn" between digressions into physics and human reproduction. Unfortunately the game's MarioKart mechanic is not discussed.`,
     releasedOn: "2023-03-05",
     fileUrl:
       "https://boysticks-podcast.s3.amazonaws.com/episodes/01_Horizon_Forbidden_West.mp3",
