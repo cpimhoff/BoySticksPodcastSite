6a80b85eb731b4f0ac0f272c01e44728415fafb3
Dylan Forbes <dylandforbes@gmail.com>
2025-01-01 16:49:58 -0800

    publish draft of rebirth part 3

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -2,6 +2,13 @@ import { Episode, RssChannel } from "./types";
 
 const ALL_EPISODES: Episode[] = [
   {
+    title: "Final Fantasy VII Rebirth (Part 3)",
+    slug: "final-fantasy-vii-rebirth-part-3",
+    description: `todo`,
+    releasedOn: null,
+    fileUrl: "https://boysticks-podcast.s3.amazonaws.com/episodes/18_Final_Fantasy_VII_Rebirth_3.mp3"
+  },
+  {
     title: "Final Fantasy VII Rebirth (Part 2)",
     slug: "final-fantasy-vii-rebirth-part-2",
     description: `todo`,
